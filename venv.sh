a6cf0bd0ff8dfae9efbfd1358bc6d84d1c8a4bc2
DouProjo <datla.srv@gmail.com>
2025-08-28 08:58:35 -0700

    New

---
+++ venv.sh
@@ -0,0 +1,6 @@
+# Python virtual environment setup script
+# Usage: source venv.sh
+
+python3 -m venv venv
+source venv/bin/activate
+echo "Virtual environment activated. To deactivate, run: deactivate"
